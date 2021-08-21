cb94e370dfc67e9d155116a56edf69835008871c
pch904 <prosenjit.chd@gmail.com>
2021-08-21 19:43:47 +0600

    secondUpdate

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,10 @@
-// update cost function
-function updateMainCost(option, price){
-    const previousCost = document.getElementById(option + '-cost');
-    const previousCostNumber = parseInt(previousCost.innerText);
-
-    previousCost.innerText = price;
-
-    calculateTotalPrice(option, previousCostNumber);
-}
-
 // calculate total price function
 function calculateTotalPrice(option, previousPrice){
     const previousTotal = document.getElementById('total-price');
     const previousTotalNumber = parseInt(previousTotal.innerText);
-
     const currentBaseCost = document.getElementById(option + '-cost');
     const currentPriceNumber = parseInt(currentBaseCost.innerText);
-
     let currentMainTotal = 0;
-
-    console.log(previousPrice, parseInt(currentBaseCost.innerText));
 
     if (previousPrice < currentPriceNumber){
         currentMainTotal = previousTotalNumber + (currentPriceNumber - previousPrice);
@@ -29,31 +15,30 @@ function calculateTotalPrice(option, previousPrice){
     else{
         currentMainTotal = previousTotalNumber;
     }
-    
     previousTotal.innerText = currentMainTotal;
-
     //update TOTAL work here
     document.getElementById('total-final-price').innerText = currentMainTotal;
 }
-
 // using promo then calculate final price 
 function applyPromo(code){
     const totalSetCost = document.getElementById('total-price');
     const totalCostNumber = parseFloat(totalSetCost.innerText);
-
     const finalMainPrice = document.getElementById('total-final-price');
     const finalMainPriceNumber = parseFloat(finalMainPrice.innerText);
-
     const promoInput = document.getElementById('promo-code');
-    
     const afterApplyCode = finalMainPriceNumber - (finalMainPriceNumber * 0.2)
-
     if (code == promoInput.value && finalMainPriceNumber == totalCostNumber){
         finalMainPrice.innerText = afterApplyCode;
     }
     promoInput.value = '';
 }
-
+// update cost function
+function updateMainCost(option, price){
+    const previousCost = document.getElementById(option + '-cost');
+    const previousCostNumber = parseInt(previousCost.innerText);
+    previousCost.innerText = price;
+    calculateTotalPrice(option, previousCostNumber);
+}
 // memory even handaler
 document.getElementById('memory-8GB').addEventListener('click', function(){
 
@@ -63,7 +48,6 @@ document.getElementById('memory-16GB').addEventListener('click', function(){
     
     updateMainCost('memory', 180);
 })
-
 // ssd even handaler
 document.getElementById('ssd-256BG').addEventListener('click', function(){
     
@@ -77,7 +61,6 @@ document.getElementById('ssd-1TB').addEventListener('click', function(){
     
     updateMainCost('ssd', 180);
 })
-
 // delivery even handaler
 document.getElementById('delivery-free').addEventListener('click', function () {
     
@@ -87,7 +70,6 @@ document.getElementById('delivery-pay').addEventListener('click', function () {
     
     updateMainCost('delivery', 20);
 })
-
 //promo code even handaler
 document.getElementById('promo-button').addEventListener('click', function(){
     applyPromo('stevekaku');
